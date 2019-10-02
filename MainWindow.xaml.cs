9437d37521c1b426e483e5fd418eaca0cfa96bd1
NamHaYeong <51144178+NamHaYeong@users.noreply.github.com>
2019-10-02 16:57:29 +0900

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,131 +14,130 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Quick
+namespace Closest_Pair
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
+    /// 
+
+    //IComparer는 비교를 위한 인터페이스이다.
+    public class XComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return (int)(((Point)x).X - ((Point)y).X);
+        }
+    }
+    public class YComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return (int)(((Point)x).Y - ((Point)y).Y);
+        }
+    }
+
     public partial class MainWindow : Window
     {
-        const int SIZE = 100000;  // 데이터 갯수
-        const int MAX = 1000000; // 랜덤 숫자의 상한
-
-        int[] a = new int[SIZE];
-        Random r = new Random();
-        private int PartitionSize = 0;
-
+        const int p = 100;
+        Point[] points = new Point[p];
         public MainWindow()
         {
-            InitializeComponent();
-            MakeRandomArray();
-            PrintArray();
+           
         }
 
-        private void PrintArray()
-        {
-            textBox.Text += "\n";
-            for (int i = 0; i < 10; i++)
-                textBox.Text += a[i] + " ";
-            textBox.Text += "...";
-            for (int i = SIZE - 10; i < SIZE; i++)
-                textBox.Text += a[i] + " ";
-
-
-        }
-
-        private void MakeRandomArray()
-        {
-            for (int i = 0; i < SIZE; i++)
-                a[i] = r.Next(MAX);
-        }
-        //QuickSort 버튼 클릭
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            for (PartitionSize = 0; PartitionSize < 10; PartitionSize++)
+            Canvas1.Children.Clear();
+            MakePointArray();
+        }
+
+        private void MakePointArray()
+        {
+            Random r = new Random();
+            for (int i = 0; i < p; i++)
             {
-                MakeRandomArray();
-                var watch = System.Diagnostics.Stopwatch.StartNew();
-                QuickSort(a, 0, SIZE - 1);
-                watch.Stop();
-                textBox.Text += " \n " +PartitionSize+": "+ watch.ElapsedMilliseconds + " ms ";
-                PrintArray();
+                points[i].X = r.Next(500);
+                points[i].Y = r.Next(500);
+            }
+            foreach(var p in points)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Width = 3;
+                rect.Height = 3;
+                rect.Stroke = Brushes.Black;
+                Canvas.SetLeft(rect, p.X);
+                Canvas.SetTop(rect, p.Y);
+                Canvas1.Children.Add(rect);
             }
         }
 
-        private void QuickSort(int[] a, int left, int right)
+        private void btnFind_Click(object sender, RoutedEventArgs e)
         {
-            if (left < right)
-            {
-                if (right - left > PartitionSize)
-                {
-                    int p = Partition(a, left, right);
+            //double min = double.MaxValue;
+            //for (int i = 0; i < p; i++)
 
-                    QuickSort(a, left, p - 1);
-                    QuickSort(a, p + 1, right);
-                }
-                else
-                    SlowSort(a, left, right);
-            }
+            //    for (int j = i + 1; j < p; j++)
+            //    {
+            //        double d = Dist(i, j);
+            //        //d가 min보다 작으면
+            //        //d, i, j를 저장
+            //        if(d < min)
+            //        {
+            //            d = min;
+
+            //        }
+            //    }
+            //points[] 배열에 있는 점들을
+            //x좌표를 기준으로 정렬하여 출력하시오
+            //int[] a = new int[100];
+            //Random r = new Random();
+            //for (int i = 0; i < 100; i++)
+            //    a[i] = r.Next(1000);
+
+            //foreach (var v in a)
+            //    Console.WriteLine(v);
+            //Console.WriteLine("...After Sort");
+            //Array.Sort(a);
+            //foreach (var v in a)
+            //    Console.WriteLine(v);
+
+            IComparer xComp = new XComparer();
+            Array.Sort(points, xComp);
+            //Array.Sort(points, new YComparer());
+            PrintPoint();
+
+           ClosestPair CP =  FindClosestPair(points, 0, 100, -1);
         }
 
-        private int Partition(int[] a, int left, int right)
+        private ClosestPair FindClosestPair(Point[] points, int right, int left, int v3)
         {
-            // 피봇을 A[left]~A[right] 중에서 선택하고, 
-            int pivot = a[left];
-            // 피봇을 A[left]와 자리를 바꾼 후, 
-            // 피봇과  배열의 각 원소를 비교하여 피봇보다 
-            // 작은  숫자들은 A[left]~A[p - 1]로 옮기고, 
-            // 피봇보다  큰 숫자들은 A[p + 1]~A[right]로 옮기며,
-            int p = left++;
-            int upper = right;
-            int lower = left;
+            if (right - left <= 3)
+                AlgorithmN2();
+            int mid = left + (right - left) / 2; //중앙점
+            ClosestPair CPL = FindClosestPair(points, left, mid);
+            ClosestPair CPR = FindClosestPair(points, mid + 1, right);
+            double d = Math.Min(CPL.dist, CPR.dist);
+            ClosestPair CPC = FindMidRange(points, d);
 
-            while(true)
-            {
-                while (left <= upper && a[left] <= pivot)
-                    left++;
-                while (right >= lower && a[right] >= pivot)
-                    right--;
-                //left 와 right 인덱스의 값을 바꿈
-                if (left < right)
-                {
-                    int t = a[left];
-                    a[left] = a[right];
-                    a[right] = t;
-                }
-                else
-                    break;
-            }
-            // 피봇은 A[p]에 놓는다.
-            int tmp = a[p];
-            a[p] = a[right];
-            a[right] = tmp;
-            return right;
-
+            return MinCP(CPL, CPR, CPC);
         }
 
-        private void SlowSort_Click(object sender, RoutedEventArgs e)
+        class ClosestPair
         {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            SlowSort(a, 0, SIZE - 1);
-            watch.Stop();
-            textBox.Text += " \n " + watch.ElapsedMilliseconds + " ms ";
-            PrintArray();
+            //가장 가까운 거리, 두 점
+        }
+        private void PrintPoint()
+        {
+            foreach (var p in points)
+                Console.WriteLine(p.X + " , " + p.Y);
         }
 
-        private void SlowSort(int[] a, int left, int right)
-        {
-            for (int i = left; i < right; i++)
-                for (int j = i+1; j <= right; j++)
-                    if(a[i]> a[j])
-                {
-                        int t = a[i];
-                        a[i] = a[j];
-                        a[j] = t;
-                }
-
-            
-        }
+        //points
+        //private double Dist( int i,  int j)
+        //{
+        //    return Math.Sqrt((points[i].X - points[j].X) * (points[i].X - points[j].X) +
+        //         (points[i].Y - points[j].Y) * (points[i].Y - points[j].Y));
+        //}
     }
 }
